f2c4c4468738957f6849e8202e84135f993aa75a
eeburgos <estebanedgardo.burgos@everis.nttdata.com>
2021-12-12 19:50:55 -0300

    add api key

--- dashboard-mf/src/utils/useQueryCryptos.ts
+++ dashboard-mf/src/utils/useQueryCryptos.ts
@@ -1,13 +1,12 @@
 import { useQuery } from "react-query";
 import { CoinFullInfo } from "../types";
-import { RAPIDAPI_KEY } from "./const";
 
 async function fetchCryptos(page: number, count: number) {
     const response = await fetch(`https://coingecko.p.rapidapi.com/coins/markets?vs_currency=usd&page=${page}&per_page=${count}&order=market_cap_desc`, {
         method: "GET",
         headers: {
             "x-rapidapi-host": "coingecko.p.rapidapi.com",
-            "x-rapidapi-key": `${RAPIDAPI_KEY}`
+            "x-rapidapi-key": "26496942d1msh975fcb8f3965c14p19bfb9jsnce8c17126755"
         }
     });
     const cryptos = await response.json()
